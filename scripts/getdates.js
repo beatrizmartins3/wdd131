6a29d2615a0c26c5ad4b2207ec7710521faabc8a
beatrizmartins3 <bmartins1@byupathway.edu>
2026-09-05 16:36:20 -0300

    project

---
+++ scripts/getdates.js
@@ -0,0 +1,15 @@
+
+const currentYear=new Date().getFullYear();
+document.getElementById("currentyear").textContent=currentYear;
+
+const lastModified = new Date(document.lastModified);
+const newDate=lastModified.toLocaleDateString("en-US", {
+    hour12:"true",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+});
+document.getElementById("lastmodified").textContent=newDate;
